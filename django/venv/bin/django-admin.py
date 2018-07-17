fe4e5c8a0e7649851f614d034e526cc17b31db3e
Samuel dial <samueldial@gmail.com>
2018-07-17 18:33:18 -0300

    Aprenda django 2.0 em 30 minutos arquivos

---
+++ django/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/samueldial/GitHub/Python/PythonDocStudying/django/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
